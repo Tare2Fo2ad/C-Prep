75ecd5728cf64f3f670d83981c15962fec3a2d1e
Tarek <Tarek@Tarek>
2022-03-08 00:42:57 +0200

    reverse a number

--- reverse_a_number/main.c
+++ reverse_a_number/main.c
@@ -1,11 +1,18 @@
-/*
- * main.c
- *
- *  Created on: Mar 8, 2022
- *      Author: Tarek
- */
+#include <stdio.h>
 
+int reverse(int x) {
+	int result = 0;
+	int reminder;
+	while (x != 0) {
+		reminder = x % 10;
+		result = result * 10 + reminder;
+		x /= 10;
+	}
+	return result;
+}
 
-int main(void){
-
+int main(void) {
+	int x = 6745;
+	printf("%d", reverse(x));
+	return 0;
 }
